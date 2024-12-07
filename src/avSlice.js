bc34b62ba57f91a427c88d4057799e02fd31e9c4
cristian <cristian.triboi@gmail.com>
2024-12-07 17:52:58 -0500

    Fix some little tipo

--- src/avSlice.js
+++ src/avSlice.js
@@ -36,24 +36,20 @@ export const avSlice = createSlice({
     ],
 
 
-  reducers: {
-    incrementAvQuantity: (state, action) => {
-        const item = state[action.payload];
-        if (item) {
-            item.quantity++;
-        }
-      
-    },
-    decrementAvQuantity: (state, action) => {
-        const item = state[action.payload];
-        if (item && item.quantity > 0) {
-            item.quantity--;
-        }
-     
-    },
-  },
-});
-
+    reducers: {
+        incrementAvQuantity: (state, action) => {
+            const item = state[action.payload];
+            if (item) {
+                item.quantity++;
+            }
+        },
+        decrementAvQuantity: (state, action) => {
+            const item = state[action.payload];
+            if (item && item.quantity > 0) {
+                item.quantity--;
+            }
+        },
+      },
+    });
 export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
-
 export default avSlice.reducer;
